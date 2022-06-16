558aa8be1b4a2b7aac92ea176db90e8e252cad57
Anthony Connell <toco85@gmail.com>
2022-06-15 20:01:27 -0400

    Finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
